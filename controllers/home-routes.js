c5b56e83efa0b889523d97b52665d975bb05c12f
Alexandra Holden <alexandraholden@Alexandras-MacBook-Air.local>
2022-03-26 13:38:49 -0400

    fixed stylesheet and js routes in handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-
+      console.log(posts);
       res.render('homepage', {
         posts,
         loggedIn: req.session.loggedIn
